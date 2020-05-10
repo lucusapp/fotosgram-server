9c855b1a82d834a344b08f6acc7b77fc89530e52
unknown <lucusapp@gmail.com>
2020-05-10 17:18:01 +0200

    215. Generar un nombre único a la imagen

--- clases/file-system.ts
+++ clases/file-system.ts
@@ -1,6 +1,7 @@
 import { FileUpload } from "../interfaces/file-upload";
 import path from 'path';
-import fs from 'fs'
+import fs from 'fs';
+import uniqid from 'uniqid';
 
 
 
@@ -10,8 +11,24 @@ export default class FileSystem{
     constructor(){ };
 
     guardarImagenTemporal(file:FileUpload,userId:string){
+        //CREAR CARPETAS
         const path = this.crearCarpetaUsuario(userId)
- 
+        //NOMBRE ARCHIVO
+        const nombreArchivo = this.generarNombreUnico(file.name);
+        console.log(nombreArchivo);
+        console.log(file.name);
+        
+    
+    }
+
+    private generarNombreUnico(nombreOriginal:string){
+        const nombreArr = nombreOriginal.split('.');
+        const extension = nombreArr[nombreArr.length -1];
+
+        const idUnico =uniqid();
+
+        return `${idUnico}.${extension}`;
+        
     }
     private crearCarpetaUsuario(userId:string){
         const pathUser = path.resolve( __dirname, '../uploads', userId);
--- dist/clases/file-system.js
+++ dist/clases/file-system.js
@@ -5,12 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var path_1 = __importDefault(require("path"));
 var fs_1 = __importDefault(require("fs"));
+var uniqid_1 = __importDefault(require("uniqid"));
 var FileSystem = /** @class */ (function () {
     function FileSystem() {
     }
     ;
     FileSystem.prototype.guardarImagenTemporal = function (file, userId) {
+        //CREAR CARPETAS
         var path = this.crearCarpetaUsuario(userId);
+        //NOMBRE ARCHIVO
+        var nombreArchivo = this.generarNombreUnico(file.name);
+        console.log(nombreArchivo);
+        console.log(file.name);
+    };
+    FileSystem.prototype.generarNombreUnico = function (nombreOriginal) {
+        var nombreArr = nombreOriginal.split('.');
+        var extension = nombreArr[nombreArr.length - 1];
+        var idUnico = uniqid_1.default();
+        return idUnico + "." + extension;
     };
     FileSystem.prototype.crearCarpetaUsuario = function (userId) {
         var pathUser = path_1.default.resolve(__dirname, '../uploads', userId);
